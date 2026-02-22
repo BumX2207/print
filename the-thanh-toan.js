84cd037620589f03c57b67770c66f44c175c399b
BumX2207 <congtunho209@gmail.com>
2026-02-22 14:44:00 +0700

    Update the-thanh-toan.js

--- the-thanh-toan.js
+++ the-thanh-toan.js
@@ -1,1 +1,12 @@
-
+{
+    id: 'tk-qr',
+    name: '💳 Tem Thanh Toán (GitHub)',
+    bg: 'https://admintnb.com/wp-content/uploads/2026/01/Tet-1.png', 
+    width: 700, 
+    height: 500,
+    inputs: [
+        { label: 'Bank', x: 50, y: 50, w: 600, size: 24, color: '#2ecc71', bold: true, val: 'VIETCOMBANK' },
+        { label: 'STK', x: 50, y: 100, w: 600, size: 40, color: '#000', bold: true, val: '9999.8888.6666' },
+        { label: 'Tên', x: 50, y: 160, w: 600, size: 24, color: '#333', val: 'NGUYEN VAN A' }
+    ]
+}
